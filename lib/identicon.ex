f0221ee761ca9a18db0e0f5ed540b5ca006ae433
Dmitry Polisadov <dmitry@polisadov.ru>
2017-09-28 17:31:57 +0300

    add filter to remove all odd values from grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,8 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -20,12 +22,28 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    grid = hex
+           |> Enum.chunk(3)
+           |> Enum.map(&mirror_row/1)
+           |> List.flatten
+           |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row([first, second | _tail] = row) do
     row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+
+    grid = Enum.filter grid, fn ({code, _index}) ->
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    grid
   end
 end
